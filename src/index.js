b3008f782856693b79f59cce361e6b186e83fe12
mar-rin <125647550+mar-rin@users.noreply.github.com>
2023-07-21 19:28:58 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './routes/App';
-import reportWebVitals from './reportWebVitals';
-import WebForms from "./routes/Forms";
 
 import {
     createBrowserRouter,
@@ -17,10 +15,6 @@ const router = createBrowserRouter([
         element: <App />,
         errorElement: <ErrorPage />
     },
-    {
-        path: "/waitlist",
-        element: <WebForms />,
-    }
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -32,7 +26,4 @@ root.render(
     </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
